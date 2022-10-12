b32f204e84f879c57820e64fb5f41987c2edc1fb
cds <1104064233@qq.com>
2022-10-12 15:31:25 +0800

    init

---
+++ src/main/java/cct/cds/cxdict/CxdictApplication.java
@@ -0,0 +1,13 @@
+package cct.cds.cxdict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CxdictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CxdictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cct/cds/cxdict/CxdictApplicationTests.java
@@ -0,0 +1,13 @@
+package cct.cds.cxdict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxdictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
