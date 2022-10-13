b1bd2cada35573d40988c836d47d7ee42e08c786
cds <1104064233@qq.com>
2022-10-13 15:22:34 +0800

    init

--- src/main/java/cct/cds/cxdict/component/RestAuthenticationEntryPoint.java
+++ src/main/java/cct/cds/cxdict/component/RestAuthenticationEntryPoint.java
@@ -1,5 +1,25 @@
 package cct.cds.cxdict.component;
 
-public class RestAuthenticationEntryPoint {
-    
+import cn.hutool.json.JSONUtil;
+import cct.cds.cxdict.common.api.CommonResult;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 自定义返回结果：未登录或登录过期
+ */
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control","no-cache");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().println(JSONUtil.parse(CommonResult.unauthorized(authException.getMessage())));
+        response.getWriter().flush();
+    }
 }
